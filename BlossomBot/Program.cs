7ea8e6c18f5709e4e0a53a254d65879f0ca5f1fa
PixelQuinn <quinn.reams@icloud.com>
2023-11-12 23:38:22 -0500

    feat: json implemented with safe token measures

--- BlossomBot/Program.cs
+++ BlossomBot/Program.cs
@@ -1,16 +1,27 @@
-﻿using System;
+﻿// Import necessary libraries
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+// Define the namespace for your bot
 namespace BlossomBot
 {
+    // Main class for your bot
     internal class Program
     {
+        // Properties for Discord client and CommandsNext extension
+        private static DiscordClient Client { get; set; }
+        private static CommandsNextExtension Commands {  get; set; }
+
+        // Main method, the entry point for your bot
         static void Main(string[] args)
         {
-            private static DiscordClient Client( get;)
+
         }
     }
 }
---
+++ BlossomBot/config/configlink.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Config file to read from output so your token is not exposed
+namespace BlossomBot.config
+{
+    internal class configlink
+    {
+        // Properties to store token and prefix
+        public string token {  get; set; }
+        public string prefix { get; set; }
+
+        // Method to read configuration from JSON file
+        public async Task ReadJSON()
+        {
+            // Using statement to automatically close the StreamReader when done
+            using (StreamReader sr = new StreamReader("config.json"))
+            {
+                string json = await sr.ReadToEndAsync();
+            }
+        }
+    }
+
+    // Class representing the structure of JSON configuration
+    internal sealed class JSONStructure
+    {
+        public string token { set; get; }
+        public string prefix { set; get; }
+    }
+}
