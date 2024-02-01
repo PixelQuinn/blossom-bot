d71ee4fe9357c614465a0d39f5e9424a0fc501bf
PixelQuinn <quinn.reams@icloud.com>
2024-02-01 11:23:15 -0500

    feat: google image search slash command started

--- BlossomBot/commands/SlashCommands/SCCommands.cs
+++ BlossomBot/commands/SlashCommands/SCCommands.cs
@@ -1,14 +1,42 @@
 ﻿using DSharpPlus.SlashCommands;
+using Google.Apis.CustomSearchAPI.v1;
+using Google.Apis.Services;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace BlossomBot.commands.SlashCommands
 {
     public class SCCommands : ApplicationCommandModule
     {
-        [SlashCommand("test", "This is my first slash command.")]
-        public async Task MyFirstSlashCommand(InteractionContext ctx)
+        [SlashCommand("image", "Google image search.")]
+        public async Task GoogleImageSearch(InteractionContext ctx, [Option("search", "Your search query")] string search)
         {
-            await ctx.Channel.SendMessageAsync("Hello World!");
+            await ctx.DeferAsync();
+
+            string apiKey = "insert api key here";
+            string cseId = "insert custom search engine id";
+
+            var customSearchService = new CustomSearchAPIService(new BaseClientService.Initializer()
+            {
+                ApiKey = apiKey,
+                ApplicationName = "Blossombot"
+            });
+
+            var listRequest = customSearchService.Cse.List();
+            listRequest.Cx = cseId;
+            listRequest.SearchType = CseResource.ListRequest.SearchTypeEnum.Image;
+            listRequest.Q = search;
+
+            var searchRequest = await listRequest.ExecuteAsync();
+            var results = searchRequest.Items;
+
+            if (results == null || results.Any())
+            {
+                await ctx.EditResponseAsync(new DSharpPlus.Entities.DiscordWebhookBuilder().WithContent("No results found."));
+                return;
+            }
+
+
         }
     }
 }
